6eb279c85fd6a2bd516a51fce76bb1947b22bc46
LGruvberg <love.gruvberg@stud.sti.se>
2025-10-25 01:58:25 +0200

    Updated main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "DataManager.h"
+
+void interface();
+
+int main() {
+    DataManager dm;
+    int choice = 0;
+    do {
+        interface();
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1: {
+            int limit;
+            std::cout << "How many values would you like to add?\n\n\tAnswer:\t";
+            std::cin >> limit;
+            dm.setValueAmountLimit(limit);
+            dm.dataEntry();
+            break;
+        }
+        case 2: std::cout << std::endl; dm.statFunc(); break;
+        case 3: dm.searchFunc(); break;
+        case 4: dm.sortFunc(); break;
+        //case 5: dm.saveToFileFunc(); break;
+        case 6: std::cout << "\nExiting...\n"; break;
+        default: 
+            std::cout << "Invalid option.\n"; break;
+        }
+    } while (choice != 6);
+
+    return 0;
+}
+void interface() {
+    std::cout << "\n1. Add values\n2. Statistics\n3. Search\n4. Sort\n5. Save to .csv\n6. EXIT\n\n\tAnswer:\t";
+}
