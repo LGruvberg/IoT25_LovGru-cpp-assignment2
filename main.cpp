58848e54cc7d877bddccb6489a0be2c637e4ebf7
LGruvberg <love.gruvberg@stud.sti.se>
2025-10-28 09:47:01 +0100

    Added bmiCalc to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*  LOOK HERE
-Compile using the following commands
+Compile using:
 1.  'g++ main.cpp DataManager.cpp -o mainExecutable'
 2.  and then '.\mainExecutable'
 */
@@ -13,14 +13,14 @@ int main() {
     DataManager dm;
     int choice = 0;
     do {
-        interface();
+        dm.interface();
         std::cin >> choice;
         
 
         switch (choice) {
         case 1: {
             int limit;
-            std::cout << "How many values would you like to add?\n\n\tAnswer:\t";
+            std::cout << "How many values would you like to add?\n\n\tChoice:\t";
             std::cin >> limit;
             dm.setValueAmountLimit(limit);
             dm.dataEntry();
@@ -31,6 +31,7 @@ int main() {
         case 4: dm.sortFunc(); break;
         case 5: dm.convertIntoFile(); break;
         case 6: std::cout << "\nExiting...\n"; break;
+        case 9: dm.bmiCalc(); break;
         default: 
             std::cout << "Invalid option.\n"; break;
             continue;
@@ -38,7 +39,4 @@ int main() {
     } while (choice != 6);
 
     return 0;
-}
-void interface() {
-    std::cout << "\n1. Add values\n2. Statistics\n3. Search\n4. Sort\n5. Save to .csv\n6. EXIT\n\n\tAnswer:\t";
 }
